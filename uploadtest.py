ea89fd4d990c05547a0b8f860bdd1ce360453c22
alexcore <nemshilov90@gmail.com>
2020-10-28 23:43:46 +0200

    Тест  upload

--- uploadtest.py
+++ uploadtest.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
-
+import os
+import time
 
 class testupload(unittest.TestCase):
     driver = webdriver.Chrome()
@@ -10,6 +11,14 @@ class testupload(unittest.TestCase):
 
     def testupload(self):
         self.driver.get('http://suninjuly.github.io/file_input.html')
+        self.driver.find_element_by_css_selector('input.form-control:nth-child(2)').send_keys("Huillo")
+        self.driver.find_element_by_css_selector('input.form-control:nth-child(4)').send_keys("Zalupovich")
+        self.driver.find_element_by_css_selector('input.form-control:nth-child(6)').send_keys("ZalupovichHuillo@gmail.com")
+        current_dir = os.path.abspath(
+            os.path.dirname('/Users/alexcore/Desktop/NEMSHYLOV.txt'))  # получаем путь к директории текущего исполняемого файла
+        file_path = os.path.join(current_dir, 'NEMSHYLOV.txt')  # добавляем к этому пути имя файла
+        self.driver.find_element_by_css_selector('#file').send_keys(file_path)
+        self.driver.find_element_by_css_selector('.btn').click
 
 
     def tearDown(self):
