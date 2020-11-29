82e1805a2bdb40badddc4a67d9907abcb2a7d9b6
alexcore <nemshilov90@gmail.com>
2020-11-29 19:46:26 +0200

    works ofr ona and two pages without 500 check and links with home in url

--- two_page working.py
+++ two_page working.py
@@ -1,42 +1,66 @@
-import time
-import unittest
+import pytest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from links import links
+from links import links_dev8
+import time
 from datetime import datetime
 import random
-from links import links
-
-class testwaits(unittest.TestCase):
-    driver = webdriver.Firefox()
-    def SetUp(self):
-        self.driver.get('http://suninjuly.github.io/math.html')
 
 
-    def testsome(self):
-        self.driver.implicitly_wait(20)
+class Test_offers():
+
+
+    @classmethod
+    def setup_class(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        return self.driver
+
+
+    def test_pages(self):
         i = 0
-        link = links()
-        print(link)
+        link = links_dev8()
         while i < len(link):
-            self.driver.get(link[i])
-            self.driver.find_element_by_css_selector("#optin-form-first_name").send_keys("test")
-            self.driver.find_element_by_css_selector("#optin-form-email").send_keys("test_" + datetime.now().strftime("%m.%d.%Y_%H.%M.%S") + "@qa.team")
-            self.driver.find_element_by_css_selector("#optin-form-btn").click()
-            self.driver.find_element_by_css_selector("#reg-form-last_name").send_keys("test")
-            self.driver.find_element_by_css_selector("#reg-form-phone_number").send_keys("19212345{}".format(random.randint(10, 99)))
-            self.driver.find_element_by_css_selector("#reg-form-btn").click()
-            self.driver.find_element_by_css_selector(".timer")
+            self.driver.get("http://" + link[i])
+            url = self.driver.current_url
+            if url.__contains__("sign-up"):
+                try:
+                    self.driver.execute_script("window.scrollBy(0, 100);")
+                    self.driver.find_element_by_css_selector("#reg-form-btn").click()
+                    time.sleep(5)
+                    print(link[i] + "one")
+                    i += 1
+                except:
+                    self.driver.execute_script("window.scrollBy(0, 100);")
+                    self.driver.find_element_by_css_selector("#reg-form1-btn").click()
+                    time.sleep(5)
+                    print(link[i] + "one")
+                    i += 1
 
-            print(str(i) + str(link[i]) + '_TEST PASS')
+            else:
+                try:
+                    self.driver.find_element_by_css_selector("#optin-form-first_name").send_keys("test")
+                    self.driver.find_element_by_css_selector("#optin-form-email").send_keys(
+                        "test_" + datetime.now().strftime("%m.%d.%Y_%H.%M.%S") + "@qa.team")
+                    self.driver.find_element_by_css_selector("#optin-form-btn").click()
+                    self.driver.find_element_by_css_selector("#reg-form-btn").click()
 
-            i += 1
+                except:
+                    self.driver.find_element_by_xpath('(//*[@id="optin-form-first_name"])[2]').send_keys("test")
+                    self.driver.find_element_by_xpath('(//*[@id="optin-form-email"])[2]').send_keys("test_" + datetime.now().strftime("%m.%d.%Y_%H.%M.%S") + "@qa.team")
+                    self.driver.find_element_by_xpath('(//*[@id="optin-form-btn"])[2]').click()
+                    self.driver.find_element_by_css_selector("#reg-form-btn").click()
+                time.sleep(5)
+                print(link[i]+ "two")
+                i += 1
 
 
-    def tearDown(self):
-            self.driver.close()
+
+    @classmethod
+    def teardown_method(self):
+        print("quit browser for test..")
+        self.driver.quit()
+
 
 if __name__ == "__main__":
- unittest.main()
+ pytest.main()
